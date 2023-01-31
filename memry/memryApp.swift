5d577555506e6740d3957819f654465eb10e9721
Zachary <zjhzingo@icloud.com>
2023-01-31 14:58:06 -0500

    Initial Commit

---
+++ memry/memryApp.swift
@@ -0,0 +1,17 @@
+//
+//  memryApp.swift
+//  memry
+//
+//  Created by Zachary Hixon on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct memryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
